31e18f2e2ddb470b5d1f74a64866ff6ad6ed4598
Teerth Satwara <tsatwar1@jh.edu>
2025-03-19 12:36:37 -0400

    pipeline-bug

--- src/routes/homePage/homepage.jsx
+++ src/routes/homePage/homepage.jsx
@@ -1,10 +1,56 @@
+
 import "./homepage.css"
+import TopBar from "../../components/topBar/topbar"
 
-import React from 'react'
+import Footer from "../../components/footer/footer"
+import {useState, useEffect} from "react"
 
+import BarChart from "../../utils/BarChart";
+import * as d3 from "d3";
 function Homepage() {
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    d3.csv("../data/chart-data.csv").then((d) => {
+      setData(d);
+      setLoading(false);
+    });
+    return () => undefined;
+  }, []);
+
   return (
-    <div>HomePage</div>
+    <>
+      <div>
+        <TopBar />
+        <Footer />
+        <section className='Info'>
+
+          <div className="left">
+
+            <span className='c-heading'>Climate Dashboard</span>
+            <p className='para'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            </p>
+
+          </div>
+          <div className="right">
+            <p className="para">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p className='btn-abt'>Go to the About Section</p>
+          </div>
+
+        </section>
+
+
+          <div className="graph">
+            {loading && <div>loading</div>}
+            {!loading && <BarChart data={data} />}
+          </div>
+
+
+      </div>
+    </>
   )
 }
 
