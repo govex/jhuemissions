54accea60b19c61f68f40c97e1ae2efd332113f8
Teerth Satwara <tsatwar1@jh.edu>
2025-03-25 03:35:57 -0400

    toggle added

--- src/components/modal/TestModal.jsx
+++ src/components/modal/TestModal.jsx
@@ -2,18 +2,27 @@ import Modal from "../../components/modal/modal";
 import ModalBody from "../../components/modal/ModalBody";
 import ModalHeader from "../../components/modal/ModalHeader";
 import ModalFooter from "../../components/modal/ModalFooter";
-
+import Button from "../button/button";
+import { ReactComponent as CloseX } from "../icons/close-x";
 function TestModal(props) {
     return (
         <Modal>
           <ModalHeader>
-            <h3>Test Modal #1</h3>
+            <h3>Filter Menu <CloseX /></h3>
           </ModalHeader>
           <ModalBody>
             <p>Body of modal #1</p>
           </ModalBody>
           <ModalFooter>
-            <button onClick={ props.close } className="btn btn-primary">Close Modal</button>
+          <Button 
+            type="solid"
+            
+            text="Apply"
+            color="secondary"
+            
+            onClick={props.close}
+            
+          />
           </ModalFooter>
         </Modal>
       );
