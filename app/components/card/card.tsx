c0f899fffa5b6d7baaf10e54325af79a3ede47b1
Heather Bree <me@justheatherbree.com>
2025-04-02 07:59:21 -0700

    title text style adjustment

--- app/components/card/card.tsx
+++ app/components/card/card.tsx
@@ -3,7 +3,7 @@ import styles from "./card.module.scss";
 function Card({title, children} : {title: string, children: any}) {
     return (
         <div className={styles.card}>
-        <h2>{title}</h2>
+        <h2 className={styles.cardtitle}>{title}</h2>
         {children}
         </div>
     )
