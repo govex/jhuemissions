e3a630677425e67896b70e1e44f8496cd9970a0a
Heather Bree <me@justheatherbree.com>
2025-05-15 10:57:50 -0700

    remove callback route

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,9 @@
-import { type RouteConfig, index, route, layout, redirect } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
 
 export default [
     layout("routes/layout.tsx", [
         index("routes/about/about.tsx"),
         route("dashboard", "routes/dashboard/dashboard.tsx"),
         route("methodology", "routes/methodology/methodology.tsx"),
-        route("auth/oidc/callback", "routes/dashboard/dashboard.tsx")
     ])
 ] satisfies RouteConfig;
