4bc14195b2a522149b120800a191beb831c06bf1
Heather Bree <me@justheatherbree.com>
2025-05-15 14:27:44 -0700

    try useEffect

--- app/routes/dashboard/dashboard.tsx
+++ app/routes/dashboard/dashboard.tsx
@@ -29,12 +29,16 @@ export function meta({ }: Route.MetaArgs) {
 }
 type errorText = "Only five years may be displayed at once" | "At least one year must be selected." | undefined;
 function Dashboard({}: Route.ComponentProps) {
-  const auth = useAuth();
-  if (auth.isAuthenticated) {
-    console.log(auth);
-  } else {
-    auth.signinRedirect();
-  }
+  useEffect(()=>{
+    if (typeof window !== "undefined") {
+      const auth = useAuth();
+      if (auth.isAuthenticated) {
+        console.log(auth);
+      } else {
+        auth.signinRedirect();
+      }
+    }
+  },[window])
   const rootData = useRouteLoaderData("root");
   const colorScale = d3.scaleOrdinal(["#86c8bc", "#af6e5d", "#f2c80f", "#884c7e", "#3b81ca"]);
   const [schoolData, setSchoolData] = useState<any>(rootData.bookings.school);
