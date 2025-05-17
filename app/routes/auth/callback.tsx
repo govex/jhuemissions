1d94e02ab7a8ca73b67b09722f34554dd203f7eb
Heather Bree <me@justheatherbree.com>
2025-05-16 18:51:08 -0700

    clean up

--- app/routes/auth/callback.tsx
+++ app/routes/auth/callback.tsx
@@ -1,12 +1,10 @@
 import type { Route } from "./+types/callback";
 import { useNavigate } from "react-router";
-import {useState} from "react";
 import { useAuth } from "~/provider/useAuth";
 
 function Callback({}:Route.ComponentProps) {
     const auth = useAuth();
     const navigate = useNavigate();
-    console.log(auth.isAuthenticated);
     if (auth.isAuthenticated) {
         navigate("/dashboard");
     }
